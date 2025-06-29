5a0331c8982a3bcef1c538ee13151687af9bfb25
changjunYun <163440796+changjunYun@users.noreply.github.com>
2025-06-29 12:32:12 +0900

    배포

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3692003839"
+    serviceWorkerVersion: "3506578443"
   }
 });
